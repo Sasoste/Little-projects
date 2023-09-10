0189127b6fa4e91d9d42d82f368d384195a5c786
Sasoste <121641034+Sasoste@users.noreply.github.com>
2023-09-10 21:18:26 +0200

    Correction erreur, ajout commentaires stopwatch

--- stopwatchapp/script.js
+++ stopwatchapp/script.js
@@ -1,12 +1,14 @@
 // Stopwatch fait à l'aide du tutoriel de Bro Code : https://www.youtube.com/watch?v=8Nsb9cjmOVA
 // Modification du tutoriel : 1 seul bouton pour play/pause, un bouton tour pour mettre en mémoire un temps, rajout des icones fontawesome
 
+// On récupère les éléments présents dans l'index.html
 const timeDisplay = document.querySelector("#timeDisplay");
 const toggleBtn = document.querySelector("#toggleBtn");
 const resetBtn = document.querySelector("#resetBtn");
 const lapBtn = document.querySelector("#lapBtn");
 const lapsDiv = document.querySelector("#laps");
 
+// On initialise les différentes variables qui vont nous servir
 let startTime = 0;
 let elapsedTime = 0;
 let currentTime = 0;
@@ -20,6 +22,7 @@ let ms = 0;
 let lapCount = 0;
 let lastLapTime = "";
 
+// On ajoute un écouteur de click sur le bouton, qui permet de lancer le chronomètre, et le mettre en pause s'il est lancé.
 toggleBtn.addEventListener("click", () => {
   if (paused) {
     paused = false;
@@ -28,15 +31,15 @@ toggleBtn.addEventListener("click", () => {
     toggleBtn.innerHTML = '<i class="fa-solid fa-pause"></i>';
   } else {
     paused = true;
-    elapsedTime = Date.now() - startTime;
+    elapsedTime = Date.now() - startTime; // mise à jour pour permettre de reprendre au bon moment quand c'est paused
     clearInterval(intervalId);
     toggleBtn.innerHTML = '<i class="fa-solid fa-play"></i>';
   }
 });
 
+// On ajoute un écouteur de click sur le bouton, qui permet de rajouter un tour si le temps est pas le même qu'avant
 lapBtn.addEventListener("click", () => {
   const lapTime = timeDisplay.textContent;
-
   if (lapTime !== lastLapTime) {
     lapCount++;
     const lapElement = document.createElement("div");
@@ -46,6 +49,7 @@ lapBtn.addEventListener("click", () => {
   }
 });
 
+// On ajoute un écouteur au click du bouton, qui remet à zéro le chronomètre
 resetBtn.addEventListener("click", () => {
   paused = true;
   started = false;
@@ -61,13 +65,15 @@ resetBtn.addEventListener("click", () => {
   lapCount = 0;
 });
 
+// Fonction pour mettre à jour l'affichage du temps écoulé
 function updateTime() {
-  elapsedTime = Date.now() - startTime;
+  elapsedTime = Date.now() - startTime; // Calcul du temps écoulé
 
-  ms = Math.floor((elapsedTime % 1000) / 10);
-  secs = Math.floor((elapsedTime / 1000) % 60);
+  // Math.floor pour arrondir, ce qui permet d'avoir les heures, puis le calcul pour les minutes, on arrondi pour laisser la place aux secondes etc
+  ms = Math.floor((elapsedTime % 1000) / 10); // On calcule le nombre de ms mais /10 parce que l'affichage même des millisecondes est inutile
+  secs = Math.floor((elapsedTime / 1000) % 60); // On calcule le nombre de secondes
   mins = Math.floor((elapsedTime / (1000 * 60)) % 60);
-  hrs = Math.floor((elapsedTime / (1000 * 60 * 60)) % 60);
+  hrs = Math.floor(elapsedTime / (1000 * 60 * 60));
 
   ms = pad(ms);
   secs = pad(secs);
@@ -76,6 +82,7 @@ function updateTime() {
 
   timeDisplay.textContent = `${hrs}:${mins}:${secs}:${ms}`;
 
+  // Permet d'ajuster les affichages pour avoir toujours un affichage à 2 chiffres partout
   function pad(unit) {
     return ("0" + unit).length > 2 ? unit : "0" + unit;
   }
